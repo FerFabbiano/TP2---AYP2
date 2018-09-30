92a0d5c88c70c130de518a41191f99eaa0c61f03
JoacoAndresen <38512322+JoacoAndresen@users.noreply.github.com>
2018-09-30 13:44:51 -0300

    Agrego funcion mostrarAhorcado

--- ahorcado.cpp
+++ ahorcado.cpp
@@ -35,6 +35,89 @@ int Ahorcado::obtenerIntentos(){
 	return intentos;
 }
 
+//MŽtodo obtenerFallos
+int Ahorcado::obtenerFallos(){
+    return intentos;
+}
+
+// MŽtodo mostrarAhorcado
+void Ahorcado::mostrarAhorcado(int intentos, int fallos, std::string palabra_adivinar){
+    std::string linea0 = " --------";
+    std::string linea01 = "|                 ";
+    std::string linea1 = "|      -|-";
+    std::string linea2 = "|     |   |       ";
+    std::string linea3 = "|     |___|";
+    std::string linea4 = "|       |         ";
+    std::string linea51 = "|    ---|         ";
+    std::string linea5 = "|    ---|---      ";
+    std::string linea6 = "|       |         ";
+    std::string linea71 = "|      /";
+    std::string linea7 = "|      / \\";
+    std::string linea8 = "|     /   \\";
+    std::string linea81 = "|     /";
+    
+    if (fallos == 0) {
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<palabra_adivinar<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea6<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<std::endl;
+    }
+    if (fallos == 1) {
+        std::cout<<linea1<<std::endl;
+        std::cout<<linea2<<palabra_adivinar<<std::endl;
+        std::cout<<linea3<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea4<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea6<<std::endl;
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<std::endl;
+    }
+    if (fallos == 2) {
+        std::cout<<linea1<<std::endl;
+        std::cout<<linea2<<palabra_adivinar<<std::endl;
+        std::cout<<linea3<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea51<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea6<<std::endl;
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<std::endl;
+    }
+    if (fallos == 3) {
+        std::cout<<linea1<<std::endl;
+        std::cout<<linea2<<palabra_adivinar<<std::endl;
+        std::cout<<linea3<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea5<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea6<<std::endl;
+        std::cout<<linea01<<std::endl;
+        std::cout<<linea01<<std::endl;
+    }
+    if (fallos == 4) {
+        std::cout<<linea1<<std::endl;
+        std::cout<<linea2<<palabra_adivinar<<std::endl;
+        std::cout<<linea3<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea5<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea6<<std::endl;
+        std::cout<<linea71<<std::endl;
+        std::cout<<linea81<<std::endl;
+    }
+    if (fallos == 5) {
+        std::cout<<linea1<<std::endl;
+        std::cout<<linea2<<palabra_adivinar<<std::endl;
+        std::cout<<linea3<<std::endl;
+        std::cout<<linea4<<std::endl;
+        std::cout<<linea5<<"Intentos: "<<intentos<<std::endl;
+        std::cout<<linea6<<std::endl;
+        std::cout<<linea7<<std::endl;
+        std::cout<<linea8<<std::endl;
+    }
+}
+
 // Destructor
 Ahorcado::~Ahorcado(){
 	std::cout << std::endl;
